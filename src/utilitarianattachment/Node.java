16da44bd908e0469f46f892726f1c744a5d7393a
Dan Giaime (RIT Student) <dsg6874@ad.rit.edu>
2016-12-06 15:53:09 -0500

    Added formula and further logic. TODO: Fix neighbor sets

--- src/utilitarianattachment/Node.java
+++ src/utilitarianattachment/Node.java
@@ -16,14 +16,30 @@ public class Node {
     private double myProbAttach;
     private HashSet<Node> myNeighbors;
 
+    public Node(){
+        myProbAttach = .1;
+        myNeighbors = new HashSet<Node>();
+    }
+    
     public boolean tryToAdd(Node possNeighbor, Random r) {
-        if (r.nextDouble() < getMyProbAttach()) {
-            myNeighbors.add(possNeighbor);
-            System.out.println("Added a link");
+        if (r.nextDouble() < getProbAttach()) {
+            System.out.println(myNeighbors.add(possNeighbor));
+            possNeighbor.getNeighbors().add(this);
+            System.out.println("Added a link, node now has degree " + myNeighbors.size());
+            calcProb();
             return true;
         } else {
             return false;
         }
+    }
+    
+    public void calcProb(){
+        myProbAttach = (myNeighbors.size()*Math.pow(Math.E, myNeighbors.size()*150))/150; 
+    }
+    
+    public void add(Node n){
+        myNeighbors.add(n);
+        n.getNeighbors().add(this);
     }
     
     public boolean hasNeighbor(Node possNeighbor){
@@ -40,14 +56,14 @@ public class Node {
     /**
      * @return probability of attachment
      */
-    public double getMyProbAttach() {
+    public double getProbAttach() {
         return myProbAttach;
     }
 
     /**
      * @return HashSet of neighbors
      */
-    public HashSet<Node> getMyNeighbors() {
+    public HashSet<Node> getNeighbors() {
         return myNeighbors;
     }
 }
--- src/utilitarianattachment/UtilitarianAttachment.java
+++ src/utilitarianattachment/UtilitarianAttachment.java
@@ -16,6 +16,20 @@ public class UtilitarianAttachment {
         //Random for calculating whether or not a connection occurs
         Random r = new Random();
 
+        //For initial setup, we need a triangle of nodes, 
+        //so we make, connect, and add these nodes.
+        Node one = new Node();
+        Node two = new Node();
+        Node three = new Node();
+
+        one.add(two);
+        one.add(three);
+        two.add(three);
+
+        currGraph.add(one);
+        currGraph.add(one);
+        currGraph.add(one);
+
         //Loop (size) times to create the full graph
         for (int i = 0; i < size; i++) {
             //Create a new node to insert
@@ -24,28 +38,31 @@ public class UtilitarianAttachment {
             //connect with each current node individually
             currGraph.add(n);
             for (int j = 0; j < currGraph.size(); j++) {
-            System.out.println(i + " " + j);
-                n.tryToAdd(n, r);
+                System.out.println(i + " " + j);
+                n.tryToAdd(currGraph.get(i), r);
             }
         }
         short[][] adjacencyMatrix = constructAdjacencyMatrix(currGraph);
         short[][] triangles = matMult(matMult(adjacencyMatrix, adjacencyMatrix), adjacencyMatrix);
 
+        for (int i = 0; i < currGraph.size(); i++) {
+            System.out.println("Degree of node " + i + " " + currGraph.get(i).getDegree());
+        }
     }
 
     /*
      *  Constructs adjacency matrix from current graph.
      */
-    public static short[][] constructAdjacencyMatrix(ArrayList<Node> currGraph){
+    public static short[][] constructAdjacencyMatrix(ArrayList<Node> currGraph) {
         short[][] adjacencyMatrix = new short[currGraph.size()][currGraph.size()];
         for (short i = 0; i < currGraph.size(); i++) {
             for (short j = 0; j < currGraph.size(); j++) {
-                adjacencyMatrix[i][j] = (short)((currGraph.get(i).hasNeighbor(currGraph.get(j))) ? 1 : 0);
+                adjacencyMatrix[i][j] = (short) ((currGraph.get(i).hasNeighbor(currGraph.get(j))) ? 1 : 0);
             }
         }
         return adjacencyMatrix;
     }
-    
+
     /*
      *  Returns average degree of nodes in current graph
      */
@@ -55,8 +72,8 @@ public class UtilitarianAttachment {
                 .map(n -> n.getDegree())
                 .reduce(0, (a, b) -> a + b) / currGraph.size();
     }
-    
-    public static short[][] matMult(short[][] mat1, short[][] mat2){
+
+    public static short[][] matMult(short[][] mat1, short[][] mat2) {
         short[][] outputMatrix = new short[mat1.length][mat2[0].length];
         for (int i = 0; i < mat1.length; i++) {
             for (int j = 0; j < mat2[0].length; j++) {
